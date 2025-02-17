0179ea3d623bd1dbaf633f19fa608460386d05ee
angelkrustev1 <angelkrustev07@gmail.com>
2025-02-17 19:28:32 +0200

    fix: chats fixed

--- static/javascript/chats.js
+++ static/javascript/chats.js
@@ -3,20 +3,21 @@ let userId = usersDataDivElement.getAttribute('data-user-id');
 let targetUserId = usersDataDivElement.getAttribute('data-target-user-id');
 
 messagesUrl = 'http://localhost:8000/chats/messages/' + `${userId}/${targetUserId}/`;
-let last_messages = null;
+let lastMessages = null;
 
 document.addEventListener('DOMContentLoaded', async () => {
     let messagesResponse = await fetch(messagesUrl);
-    last_messages = await messagesResponse.json();
+    lastMessages = await messagesResponse.json();
 })
 
 async function checkRefreshPage() {
     let messagesResponse = await fetch(messagesUrl)
     let messagesData = await messagesResponse.json()
-    let are_messages_equal = JSON.stringify(messagesData) === JSON.stringify(last_messages)
+    let are_messages_equal = JSON.stringify(messagesData) === JSON.stringify(lastMessages)
 
     if (!are_messages_equal) {
         location.reload();
+        lastMessages=messagesData
     }
 }
 

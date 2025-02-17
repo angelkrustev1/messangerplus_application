99f438e3024ff1339f1411f66775e890e10a1ab5
angelkrustev1 <angelkrustev07@gmail.com>
2025-02-17 20:27:06 +0200

    fix: chats url

--- staticfiles/javascript/chats.js
+++ staticfiles/javascript/chats.js
@@ -2,22 +2,27 @@ let usersDataDivElement = document.getElementById('users-data');
 let userId = usersDataDivElement.getAttribute('data-user-id');
 let targetUserId = usersDataDivElement.getAttribute('data-target-user-id');
 
-messagesUrl = 'http://localhost:8000/chats/messages/' + `${userId}/${targetUserId}/`;
-let last_messages = null;
+/*
+    IF YOU RUN THIS APP LOCALLY USE THIS URL FOR "messagesUrl":
+    http://localhost:8000/chats/messages/
+*/
+messagesUrl = 'https://messangerplus-application.onrender.com/chats/messages/' + `${userId}/${targetUserId}/`;
+let lastMessages = null;
 
 document.addEventListener('DOMContentLoaded', async () => {
     let messagesResponse = await fetch(messagesUrl);
-    last_messages = await messagesResponse.json();
+    lastMessages = await messagesResponse.json();
 })
 
 async function checkRefreshPage() {
     let messagesResponse = await fetch(messagesUrl)
     let messagesData = await messagesResponse.json()
-    let are_messages_equal = JSON.stringify(messagesData) === JSON.stringify(last_messages)
+    let are_messages_equal = JSON.stringify(messagesData) === JSON.stringify(lastMessages)
 
     if (!are_messages_equal) {
         location.reload();
     }
+    lastMessages=messagesData
 }
 
 setInterval(checkRefreshPage, 5000);
